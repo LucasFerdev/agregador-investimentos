4debc5973beb5a33fc5dbb8c95293bc19f46952a
Lucas Fernando <ferlucas074@gmail.com>
2026-01-12 23:46:26 -0300

    feat(controller): implementar endpoints GET e Post de usuario

---
+++ agregadorinvestimentos/src/main/java/com/lucas/agregadorinvestimentos/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.lucas.agregadorinvestimentos.controller;
+
+import com.lucas.agregadorinvestimentos.entity.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    @PostMapping
+    public ResponseEntity<User> createUsers(@RequestBody String body) {
+        //
+        return null:
+    }
+
+    @GetMapping("/{UserId}")
+    public ResponseEntity<User> GetUserById(@PathVariable("userId") String userId) {
+        //
+        return null:
+    }
+
+}
